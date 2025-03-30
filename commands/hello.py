9ce41063e4c1325561a677a0d0c4136e9ed0354d
luljaj <lukauljaj43@gmail.com>
2025-03-30 16:26:21 -0400

    hello.py set up
    current is on a commands.bot setup

--- commands/hello.py
+++ commands/hello.py
@@ -1,11 +1,15 @@
 from discord.ext import commands
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, Object
+import os
+
+GUILD_ID = int(os.getenv("GUILDID"))
 
 class Hello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @app_commands.command(name="hello", description="Say hi!")
+    @app_commands.guilds(Object(id=GUILD_ID))  # 👈 This is key
     async def hello(self, interaction: Interaction):
         await interaction.response.send_message("Hey there!")
 
