f51023c7ecaf3b4f870cd586ab2df9ec1cd620f1
luljaj <148261188+luljaj@users.noreply.github.com>
2025-03-30 16:50:59 -0400

    databases

---
+++ database.py
@@ -0,0 +1,21 @@
+import sqlite3
+import os
+
+db_folder = './databases'
+db_file = os.path.join(db_folder,'applications.db')
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS applications (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    reason TEXT,
+    inviter TEXT,
+    status TEXT DEFAULT 'pending'
+)
+''')
+
+
+conn.commit()
+conn.close()
