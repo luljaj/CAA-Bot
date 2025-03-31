603df77a17fe044d6c64a1ddb0bedca5a7787f2b
lukauljaj <uljajluka@gmail.com>
2025-03-31 15:58:01 -0400

    set up registry editing, approval/denial.

--- commands/register.py
+++ commands/register.py
@@ -8,6 +8,7 @@ db_folder = './databases'
 db_file = os.path.join(db_folder, 'applications.db')
 GUILD_ID = int(os.getenv("GUILDID"))
 REVIEW_CHANNEL = 1356017239039414615
+INTERN_ID = 1356349048516382781
 
 
 class FrontDoor(discord.ui.Modal):
@@ -15,7 +16,6 @@ class FrontDoor(discord.ui.Modal):
         super().__init__(title="Front Door", timeout=None)
         self.bot = bot
 
-        # Create input fields
         username = discord.ui.TextInput(
             label='Roblox Username',
             placeholder='Enter your Roblox name',
@@ -44,6 +44,10 @@ class FrontDoor(discord.ui.Modal):
         self.reason = reason
         self.inviter = inviter
 
+        username = username
+        reason = reason
+        inviter = inviter
+
     async def on_submit(self, interaction: Interaction):
         with sqlite3.connect(db_file) as conn:
             cursor = conn.cursor()
@@ -57,6 +61,7 @@ class FrontDoor(discord.ui.Modal):
                 self.inviter.value
             ))
             conn.commit()
+            applicant = interaction.user.id
 
         await interaction.response.send_message(
             f"Thank you for your application to the CAA.",
@@ -64,16 +69,35 @@ class FrontDoor(discord.ui.Modal):
         )
 
         embed = discord.Embed(
-            title="🛎️ Front Door Application",
-            color=(5647104)
-        )
+                    title="🛎️ Front Door Application",
+                    color=(5647104)
+                )
         embed.add_field(name="Discord User", value=f"<@{interaction.user.id}>", inline=False)
         embed.add_field(name="Roblox Username", value=self.username.value, inline=True)
         embed.add_field(name="Reason for Entry", value=self.reason.value, inline=False)
         embed.add_field(name="Referrer", value=self.inviter.value or "N/A", inline=False)
 
+        class ReviewMenu(discord.ui.View):
+            def __init__(self,user,rbluser,bot):
+                super().__init__()
+                self.user = user
+                self.bot = bot
+                self.rbluser = rbluser
+            @discord.ui.button(label='Approve', style=discord.ButtonStyle.green)
+            async def on_approval(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+                self.stop()
+                await interaction.response.send_message(content=f'<@{self.user.id}> ({self.rbluser}) has been promoted to Intern.')
+                intern = discord.utils.get(interaction.guild.roles, name="Intern")
+                await self.user.add_roles(intern)
+                await self.user.edit(nick=self.rbluser)
+            @discord.ui.button(label='Deny', style=discord.ButtonStyle.red)
+            async def on_denial(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+                self.stop()
+                await interaction.response.send_message(content=f'<@{self.user.id}> has been denied and kicked.')
+
+
         channel = self.bot.get_channel(REVIEW_CHANNEL)
-        await channel.send(embed = embed)
+        await channel.send(embed = embed, view = ReviewMenu(interaction.user,self.username.value,self.bot))
 
 
 class Register(commands.Cog):
