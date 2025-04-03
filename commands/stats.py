2bcdb01f45508de51e83138e2cc3d31a3734a105
luljaj <lukauljaj43@gmail.com>
2025-04-03 17:35:42 -0400

    Changed stats  to include thumbnails

--- commands/stats.py
+++ commands/stats.py
@@ -13,6 +13,17 @@ GUILD_ID = int(os.getenv("GUILDID"))
 REVIEW_CHANNEL = 1356017239039414615
 
 
+def getUserId(user):
+    user = 'larnagack'
+    try:
+        link = requests.get(f'https://www.roblox.com/users/profile?username={user}')
+    except:
+        return None
+    url =  link.url
+    lurl = url.split('/')
+    id = lurl[4]
+    return id
+
 class Stats(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -43,12 +54,18 @@ class Stats(commands.Cog):
 
         username, cash, reports, eventswon = row
         avatar_url = user.avatar.url
+        user_id = getUserId(username)
+        avatar_thumbnail = requests.get(f'https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size=420x420&format=Png')
+        if avatar_thumbnail:
+            avatar_url = avatar_thumbnail.json()["data"][0]["imageUrl"]
         rank = user.top_role
 
         embed = discord.Embed(
             title=f"📂 {user.name} - EMPLOYEE RECORD",
             color=rank.color
         )
+
+        
         inline = True
         embed.add_field(name="ROLODEX ENTRY", value="", inline=False)
         embed.add_field(name="ALIAS", value=username, inline=inline)
@@ -57,7 +74,7 @@ class Stats(commands.Cog):
         embed.add_field(name="PERFORMANCE FILE", value="", inline=False)
         embed.add_field(name="REPORTS", value=reports, inline=inline)
         embed.add_field(name="EVENT WINS", value=eventswon, inline=inline)
-        embed.add_field(name = f'Awards ({len(awards)})', value = "\n".join(awards), inline = False)
+        embed.add_field(name = f'AWARDS ({len(awards)})', value = "\n".join(awards), inline = False)
         embed.set_thumbnail(url=avatar_url)
         embed.set_footer(text = 'Custom Adversaries Association', icon_url='https://cdn.discordapp.com/icons/938810131800543333/a5572ec6502690f351ab956dd5a67d8e.png?size=1024')
 
